90a783d3eacf09f53eac7a95df3d45ce498a8473
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-10-16 15:30:10 -0500

    Prueba de texto
    Perdón me olvidé subir esto

--- Traduccion/pipeline.py
+++ Traduccion/pipeline.py
@@ -4,12 +4,14 @@ import textoABraille as tb
 
 pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract\tesseract.exe' #Cambiar por la ruta de tesseract en su computadora o comentar si ya esta en el PATH
 
-def convertirImagen(imagen):
+def convertirImagen(imagen, debug=False):
     # Convertir imagen a escala de grises
     imagen = preprocesarImagen(imagen)
     #Sacar el texto de la imagen
     texto = pytesseract.image_to_string(imagen, lang='spa')
     # Texto a braille
+    if debug:
+        print("Texto conseguido de la imagen: ", texto)
     braille = tb.convertirTextoABraille(texto)
     return braille
 
