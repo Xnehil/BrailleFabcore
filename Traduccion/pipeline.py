4251852ed81622568c3fc3e23d60df300f90a269
Xnehil <harvy.martinezm@pucp.edu.pe>
2024-10-16 12:24:16 -0500

    Prueba imagen a braille

---
+++ Traduccion/pipeline.py
@@ -0,0 +1,19 @@
+import cv2
+import pytesseract
+import textoABraille as tb
+
+pytesseract.pytesseract.tesseract_cmd = r'D:\Tesseract\tesseract.exe' #Cambiar por la ruta de tesseract en su computadora o comentar si ya esta en el PATH
+
+def convertirImagen(imagen):
+    # Convertir imagen a escala de grises
+    imagen = preprocesarImagen(imagen)
+    #Sacar el texto de la imagen
+    texto = pytesseract.image_to_string(imagen, lang='spa')
+    # Texto a braille
+    braille = tb.convertirTextoABraille(texto)
+    return braille
+
+def preprocesarImagen(imagen):
+    # Aplicar filtro de mediana
+    # imagen = cv2.medianBlur(imagen, 5)
+    return imagen
